85cb0cd38c005d9fa3707a2a355a07a1d6a18cfb
Hitesh <hiteshspatil99@gmail.com>
2021-11-12 00:38:35 +0530

    [Hitesh] add : Fibonacci Series Solution

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            int first = 0, second = 1, third = 0;
+            Console.WriteLine("Enter the number of series :");
+            int series = Convert.ToInt32(Console.ReadLine());
+            if (series <= 1)
+            {
+                Console.WriteLine(first);
+                return;
+            }
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            for (int i = 2; i < series; i++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+                Console.WriteLine(third);
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
