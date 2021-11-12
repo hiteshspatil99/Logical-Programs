464806892a5f7a04a0172a553b054954543f3eba
Hitesh <hiteshspatil99@gmail.com>
2021-11-12 23:23:09 +0530

    [Hitesh] add : Perfect Number Detection

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public void Perfect()
+        {
+            int sum = 0;
+            Console.WriteLine("Enter the number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine(num + " is a Perfect number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not a Perfect number");
+            }
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -9,7 +9,7 @@ namespace LogicalPrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Perfect Number \n 3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -18,6 +18,10 @@ namespace LogicalPrograms
                         series.Fibonacci();
                         break;
                     case 2:
+                        PerfectNumber number = new PerfectNumber();
+                        number.Perfect();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
